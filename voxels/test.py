a1e3bc0208022bb6837703a305e78e7ba915d545
Mike Stedman <github@ravuya.com>
2011-07-06 22:15:24 -0600

    Thought that'd be slower, guess not

--- voxels/test.py
+++ voxels/test.py
@@ -8,3 +8,8 @@ width = 640
 height = 480
 
 rays = raytrace.getRaysForScreen(width, height, c)
+
+# make sure we got the right number of rays
+assert(rays.length == height)
+for y in range(height - 1):
+	assert(rays[y].length == width)
