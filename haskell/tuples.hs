50fe3ceafc295c99c6c9c66a2ae09ea8430f5069
Mike <github@ravuya.com>
2011-11-28 23:00:36 +0000

    Tuples example comment

--- haskell/tuples.hs
+++ haskell/tuples.hs
@@ -1,6 +1,12 @@
-f :: Integer -> (Integer, Integer)
+f :: (Num a) => a -> (a, a)
 f x = (x - 1, 0)
 
-g :: Integer -> Integer
+g :: (Num a) => a -> a
 g x = y where
 		(y, _) = f x
+
+-- This Haskell file shows off that you can decide to "not take"
+-- any part of a tuple returned from a function. Other functional
+-- languages (like ML/F#) offer the same functionality.
+-- The key point (the only point, really) is the usage of the bottom
+-- character on line 6.
