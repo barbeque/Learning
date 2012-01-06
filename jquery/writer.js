a22cefe02778a8c0ba20ab78e73d997dd1066ae4
mike stedman <github@ravuya.com>
2012-01-05 19:00:07 -0700

    More advanced use of state now

--- jquery/writer.js
+++ jquery/writer.js
@@ -2,6 +2,8 @@ $.widget("ui.writer", {
 	_init: function() {
 		var widget = this;
 		var $widget = this.element;
+		$widget.disableSelection();
+		var toggled = false;
 		
 		// This sets up the scope for "this" inside changeWriting() properly.
 		$widget.click(function(e) {
@@ -11,10 +13,16 @@ $.widget("ui.writer", {
 	
 	changeWriting: function(e) {
 		var message = this._getMessage();
+		this.toggled = !this.toggled;
 		this.element.text(message);
 	},
 	
 	_getMessage: function() {
-		return "Hey there";
+		if(this.toggled) {
+			return "Goodbye, then";
+		}
+		else {
+			return "Hey there";
+		}
 	}
 });
