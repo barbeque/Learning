e722d64609154b225ae3d6c12696bea08bf3dc90
mike stedman <github@ravuya.com>
2012-01-05 23:52:26 -0700

    it rotates, to show off of course

--- pygame/fractal.py
+++ pygame/fractal.py
@@ -1,7 +1,6 @@
 import pygame, math, random
 
-def draw(screen):
-	theta = -math.pi / 2
+def draw(screen, theta):
 	drawBranch(screen, 10, 320, 320, 40, theta)
 	
 def drawBranch(screen, depth, originX, originY, length, theta):
@@ -11,24 +10,33 @@ def drawBranch(screen, depth, originX, originY, length, theta):
 	color.g = (int)((float(depth) / 10) * 255);
 	pygame.draw.line(screen, color, (originX, originY), (destinationX, destinationY))
 	
-	thetaOffset = 3.141592 / 4.0;#2.0 * (float(depth) / 10) + 0.1;
+	thetaOffset = 3.141592 / 4.0
 	
 	if depth > 1:
 		drawBranch(screen, depth - 1, destinationX, destinationY, length * 0.95, theta + thetaOffset)
 		drawBranch(screen, depth - 1, destinationX, destinationY, length * 0.95, theta - thetaOffset)
 		
 def main():
+	pygame.init()
 	screen = pygame.display.set_mode((640, 480))
 	running = True
+	
+	delta = 0.0
+	lastTicks = pygame.time.get_ticks()
+	startingTheta = 0
 	
 	while running:
 		event = pygame.event.poll()
 		if event.type == pygame.QUIT:
 			running = False
+			
+		delta = (pygame.time.get_ticks() - lastTicks) / 1000.0
+		lastTicks = pygame.time.get_ticks()
 		
 		screen.fill((0, 0, 0))
 			
-		draw(screen)
+		startingTheta += delta
+		draw(screen, startingTheta)
 		
 		pygame.display.flip()
 	
