f69f06ec5f5b67cd9a49d15626def458bbd99d57
mike stedman <github@ravuya.com>
2012-03-27 21:13:27 -0600

    Some additional corpus of tests

--- isAnagram.py
+++ isAnagram.py
@@ -1,3 +1,4 @@
+# checks if 'smallWord' is an anagram of 'bigWord'
 def isAnagram(bigWord, smallWord):
 	sortedBigWord = sorted(list(str.lower(bigWord)))
 	sortedSmallWord = sorted(list(str.lower(smallWord)))
@@ -15,8 +16,12 @@ def isAnagram(bigWord, smallWord):
 				break
 	return True
 
-assert isAnagram("airplane", "plane") is True, "plane is not inside airplane?"
-assert isAnagram("helicopter", "chop") is True, "chop is not inside helicopter?"
+assert isAnagram("airplane", "plane") is True, "plane is an anagram of airplane"
+assert isAnagram("helicopter", "chop") is True, "chop is an anagram of helicopter"
 assert isAnagram("nipples", "pip") is True, "double letters don't work?"
 assert isAnagram("cheese", "kuwait") is False, "kuwait is not an anagram of cheese"
-assert isAnagram("geoff", "fog") is True, "kuwait is not an anagram of cheese"
+assert isAnagram("Geoff", "fog") is True, "fog is an anagram of Geoff"
+assert isAnagram("small", "smallsmall") is False, "anagrams can't be longer than the words they are anagrams of!"
+assert isAnagram("djibouti", "about") is False, "clever seeming false anagrams will still fail!"
+assert isAnagram("barbeque", "barber") is False, "can't double up illegally!"
+assert isAnagram("Anaheim", "Hemi") is True, "mixed case works for both!"
