b9bb7c3fcf6e571e80f3c768624c5e6e0bf84d4b
mike stedman <github@ravuya.com>
2012-07-08 21:08:51 -0600

    Modularizing palette viewer for palette-based sprite editor

---
+++ palettes/palpaint.py
@@ -0,0 +1,73 @@
+import pygame, sys
+from pygame.locals import *
+
+def makePalette():
+	palette = []
+	maxR = 0
+	maxG = 0
+	maxB = 0
+
+	for i in range(0, 256):
+# 2 bits of blue
+		b = i & 0x3
+# 3 bits of green
+		g = (i & 0x1c) >> 2
+# 3 bits of red
+		r = (i & 0xe0) >> 5
+
+		r = (r / 7.0) * 255
+		g = (g / 7.0) * 255
+		b = (b / 3.0) * 255
+
+		maxR = max(r, maxR)
+		maxG = max(g, maxG)
+		maxB = max(b, maxB)
+
+		palette.append(pygame.Color(int(r), int(g), int(b)))
+
+	print "maximum values of each rgb channel:", maxR, maxG, maxB
+	return palette
+
+def cyclePalette(palette, amount):
+	newPalette = list(palette)
+	for x in range(0, len(palette)):
+		newPalette[x] = palette[(x + amount) % len(palette)]
+	return newPalette
+
+def drawPalettePicker(surface, boxSize, boxGap):
+	x = 0
+	y = 0
+
+	for c in palette:
+		pygame.draw.rect(surface, c, Rect(x, y, boxSize, boxSize))
+
+		if x + 16 + boxGap > surface.get_width():
+			x = 0
+			y += boxSize
+		else:
+			x += boxSize + boxGap
+
+palette = makePalette()
+
+pygame.init()
+surface = pygame.display.set_mode((512, 384))
+pygame.display.set_caption('palpaint')
+
+isRunning = True
+
+while isRunning:
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			isRunning = False
+		elif event.type == KEYDOWN:
+			if event.key == pygame.K_ESCAPE:
+				isRunning = False
+			else:
+				palette = cyclePalette(palette, 1)
+
+	boxSize = 16
+	boxGap = 1
+
+	drawPalettePicker(surface, boxSize, boxGap)
+
+	pygame.display.update()
