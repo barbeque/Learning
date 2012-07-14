e7829319879d0dde4a39a4d38546cdc8643ce35f
mike stedman <github@ravuya.com>
2012-07-13 23:55:54 -0600

    Basically just did the node tutorial

---
+++ node.js/example.js
@@ -0,0 +1,6 @@
+var http = require('http')
+http.createServer(function(req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.end('Hello, world\n');
+}).listen(1337, '127.0.0.1');
+console.log("Server running on http://127.0.0.1:1337");
