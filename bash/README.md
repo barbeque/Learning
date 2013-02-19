f90b756bae386ef686fd9feadcf60eb11ac49066
mike stedman <github@ravuya.com>
2013-02-18 22:27:42 -0700

    domain search bash script

---
+++ bash/README.md
@@ -0,0 +1,1 @@
+Here are some simple scripts that I've used in the past to scratch an itch. Maybe you'll find them useful too.
---
+++ bash/domain-search.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+basedomain="chart.com"
+hits=0
+
+for i in {a..z}
+do
+	result=`whois -Q ${i}${basedomain} | grep "No match"`
+	if [[ ${#result} -gt 0 ]]
+		then
+			echo $i$basedomain" is available"
+			hits=`expr $hits + 1`
+	fi
+	sleep 0.5
+done
+
+echo $hits" free domains found"
